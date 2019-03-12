4c092a3e8610596a8ae78c879b4ec574dd4ae571
Yurisa <779509411@qq.com>
2019-03-12 22:37:05 +0800

    增加了menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow, ipcMain} = require('electron')
+const {app, Browserwindowdow, ipcMain, menubar, Tray, dialog} = require('electron')
 const path = require('path')
 const url = require('url')
 const pkg = require('./package.json')
@@ -36,20 +36,23 @@ ipcMain.on('uploadChoosedFiles', async (evt, files) => {
   //     db.read().get('uploaded').insert(imgs[i]).write()
   //   }
   //   clipboard.writeText(pasteText)
-  //   window.webContents.send('uploadFiles', imgs)
-  //   if (settingWindow) {
-  //     settingWindow.webContents.send('updateGallery')
+  //   windowdow.webContents.send('uploadFiles', imgs)
+  //   if (settingwindowdow) {
+  //     settingwindowdow.webContents.send('updateGallery')
   //   }
   // }
 })
 
-// 保持一个对于 window 对象的全局引用，如果你不这样做，
-// 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
-let win
+// 保持一个对于 windowdow 对象的全局引用，如果你不这样做，
+// 当 JavaScript 对象被垃圾回收， windowdow 会被自动地关闭
+let miniwindowdow
+let settingwindowdow
+let window
+let tray
 
-function createWindow () {
+function createwindowdow () {
   // 创建浏览器窗口。
-  win = new BrowserWindow({
+  window = new Browserwindowdow({
     width: 800, 
     height: 600,
     autoHideMenuBar: true,
@@ -66,37 +69,92 @@ function createWindow () {
   // 然后加载应用的 index.html。
   // package中的DEV为true时，开启调试窗口。为false时使用编译发布版本
   if(pkg.DEV){
-    win.loadURL('http://localhost:3000/')
+    window.loadURL('http://localhost:3000/')
   }else{
-    win.loadURL(url.format({
+    window.loadURL(url.format({
       pathname: path.join(__dirname, './build/index.html'),
       protocol: 'file:',
       slashes: true
     }))
   }
 
-  // 打开开发者工具。
-  // win.webContents.openDevTools()
+  function createContextMenu() {
+    contextMenu = Menu.buildTemplate([
+      {
+        label: '关于',
+        click () {
+          dialog.showMessageBox({
+            title: 'PicGo',
+            message: 'PicGo',
+            detail: `Version: ${pkg.version}\nAuthor: Molunerfinn\nGithub: https://github.com/Molunerfinn/PicGo`
+          })
+        }
+      },
+      {
+        label: '打开详细窗口',
+        click () {
+          if (settingwindowdow === null) {
+            createSettingwindowdow()
+            settingwindowdow.show()
+          } else {
+            settingwindowdow.show()
+            settingwindowdow.focus()
+          }
+          if (miniwindowdow) {
+            miniwindowdow.hide()
+          }
+        }
+      },
+      {
+        label: '选择默认图床',
+        type: 'submenu',
+        // submenu
+      },
+      {
+        label: '打开更新助手',
+        type: 'checkbox',
+        // checked: db.get('settings.showUpdateTip').value(),
+        // click () {
+        //   const value = db.read().get('settings.showUpdateTip').value()
+        //   db.read().set('settings.showUpdateTip', !value).write()
+        // }
+      },
+      {
+        label: '重启应用',
+        click () {
+          app.relaunch()
+          app.exit(0)
+        }
+      },
+      {
+        role: 'quit',
+        label: '退出'
+      }
+    ])
+  }
 
-  // 当 window 被关闭，这个事件会被触发。
-  win.on('closed', () => {
-    // 取消引用 window 对象，如果你的应用支持多窗口的话，
-    // 通常会把多个 window 对象存放在一个数组里面，
+  // 打开开发者工具。
+  // window.webContents.openDevTools()
+
+  // 当 windowdow 被关闭，这个事件会被触发。
+  window.on('closed', () => {
+    // 取消引用 windowdow 对象，如果你的应用支持多窗口的话，
+    // 通常会把多个 windowdow 对象存放在一个数组里面，
     // 与此同时，你应该删除相应的元素。
-    win = null
+    window = null
   })
 }
 
 // Electron 会在初始化后并准备
 // 创建浏览器窗口时，调用这个函数。
 // 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow)
+app.on('ready', createwindowdow)
 
 // 当全部窗口关闭时退出。
-app.on('window-all-closed', () => {
+app.on('windowdow-all-closed', () => {
   // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
   // 否则绝大部分应用及其菜单栏会保持激活。
-  if (process.platform !== 'darwin') {
+  if (process.platform !== 'darwindow') {
     app.quit()
   }
 })
@@ -104,8 +162,8 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // 在macOS上，当单击dock图标并且没有其他窗口打开时，
   // 通常在应用程序中重新创建一个窗口。
-  if (win === null) {
-    createWindow()
+  if (window === null) {
+    createwindowdow()
   }
 })
 
