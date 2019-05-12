a397fd382e5370738a53c81d1870399b115e3b8e
Yurisa <779509411@qq.com>
2019-05-12 22:35:22 +0800

    feature:图片画廊功能完善

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Row, Col, Icon, Select, Input, Checkbox } from 'antd';
+import { Row, Col, Icon, Select, Input, Checkbox, Popconfirm, message } from 'antd';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // react过渡动画 
-import ReactBnbGallery from 'react-bnb-gallery'
+import ReactBnbGallery from 'react-bnb-gallery';
+import pasteStyle from '../../mainUtils/pasteTemplate';
 import './index.less';
 
 const Option = Select.Option;
@@ -33,14 +34,11 @@ export default class Gallery extends React.Component {
       Custom: 'Custom'
     },
     galleryOpened: false,
-    dialogVisible: false
   }
 
   componentWillMount() {
     setTimeout(() => {
-      this.setState({
-        filterList: this.getGallery()
-      })
+      this.getGallery()
     }, 200);
   }
 
@@ -58,20 +56,22 @@ export default class Gallery extends React.Component {
           return item.fileName.indexOf(searchText) !== -1
         }).reverse().value();
       this.setState({
-        images: data
+        images: data,
+        filterList: data
       })
-      return data
     } else {
       console.log('读取上传图片');
       let data = db.read().get('uploaded').slice().reverse().value();
       this.setState({
-        images: data
+        images: data,
+        filterList: data
       });
-      return data
     }
   }
 
   handlePasteStyleChange = (val) => {
+    db.read().set('settings.pasteStyle', val)
+    .write();
     this.setState({
       pasteStyle: val
     })
@@ -85,6 +85,61 @@ export default class Gallery extends React.Component {
 
   isMultiple = (obj) => {
     return Object.values(obj).some(item => item)
+  }
+
+  /**
+   * 批量删除
+   */
+  onConfirmMutiDelete () {
+    const { choosedList } = this.state;
+    if (Object.values(choosedList).some(item => item)) {
+        let files = [];
+        Object.keys(choosedList).forEach(key => {
+          if (choosedList[key]) {
+            const file = db.read().get('uploaded').getById(key).value();
+            files.push(file);
+            db.read().get('uploaded').removeById(key).write();
+          }
+        })
+        this.choosedList = {};
+        this.getGallery();
+        const obj = {
+          title: '操作结果',
+          body: '删除成功'
+        };
+        const myNotification = new window.Notification(obj.title, obj);
+        myNotification.onclick = () => {
+          return true
+        };
+    }
+  }
+
+  /**
+   * 批量复制
+   */
+  multiCopy = () => {
+    const { choosedList } = this.state;
+    if (Object.values(choosedList).some(item => item)) {
+      let copyString = ''
+      const style = db.read().get('settings.pasteStyle').value() || 'markdown'
+      Object.keys(choosedList).forEach(key => {
+        if (choosedList[key]) {
+          const item = db.read().get('uploaded').getById(key).value()
+          const url = item.url || item.imgUrl
+          copyString += pasteStyle(db, style, url) + '\n'
+          choosedList[key] = false
+        }
+      })
+      const obj = {
+        title: '批量复制链接成功',
+        body: copyString
+      }
+      const myNotification = new window.Notification(obj.title, obj)
+      electron.clipboard.writeText(copyString)
+      myNotification.onclick = () => {
+        return true
+      }
+    }
   }
 
   zoomImage = (index) => {
@@ -105,22 +160,23 @@ export default class Gallery extends React.Component {
   }
 
   /**
-   * 打开对话框
+   * 复制链接
    */
-  openDialog = (item) => {
-    const { id, imgUrl } = item;
-    this.setState({
-      imgInfo: { id, imgUrl },
-      dialogVisible: true
-    })
-  }
-
   copy = (item) => {
-
-  }
-
-  remove = (item) => {
-
+    const url = item.url || item.imgUrl;  
+    console.log('url', url);
+    const style = db.read().get('settings.pasteStyle').value() || 'markdown';
+    const copyLink = pasteStyle(db, style, url);
+    const obj = {
+      title: '复制链接成功',
+      body: copyLink,
+      icon: url
+    };
+    const myNotification = new window.Notification(obj.title, obj);
+    electron.clipboard.writeText(copyLink);
+    myNotification.onclick = () => {
+      return true
+    };
   }
 
   onCheckBoxChange = (e) => {
@@ -147,6 +203,34 @@ export default class Gallery extends React.Component {
       })
     })
     return photos
+  }
+
+  onConfirmDelete = (item) => {
+    const { id } = item;
+    const file = db.read().get('uploaded').getById(id).value();
+    db.read().get('uploaded').removeById(id).write()
+    const obj = {
+      title: '操作结果',
+      body: '删除成功'
+    }
+    const myNotification = new window.Notification(obj.title, obj)
+    myNotification.onclick = () => {
+      return true
+    }
+    this.getGallery()
+  }
+
+  onCancelDelete = (e) => {
+    message.error('Click on No');
+  }
+
+  onSearch = (value) => {
+    console.log('111')
+    this.setState({
+      searchText: value
+    }, () => {
+      this.getGallery();
+    });
   }
 
   render() {
@@ -182,17 +266,25 @@ export default class Gallery extends React.Component {
                   </Row>
                   <Row className="handle-bar" gutter={16}>
                     <Col span={12}>
-                      <Search placeholder="搜索" style={{width: '100%'}} onSearch={value => console.log(value)}/>
+                      <Search placeholder="搜索" style={{width: '100%'}} onSearch={(value) => this.onSearch(value)}/>
                     </Col>
                     <Col span={6}>
-                      <div className={`item-base copy round ${this.isMultiple(choosedList) ? 'active' : null}`}>
+                      <div className={`item-base copy round ${this.isMultiple(choosedList) ? 'active' : null}`} onClick={() => this.multiCopy()}>
                         <Icon type="file-text" /> 批量复制
                       </div>
                     </Col>
                     <Col span={6}>
-                      <div className={`item-base delete round ${this.isMultiple(choosedList) ? 'active' : null}`}>
-                        <Icon type="delete" /> 批量删除
-                      </div>
+                      <Popconfirm 
+                        title="确定要删除这张图片吗" 
+                        onConfirm={() => this.onConfirmMutiDelete()} 
+                        onCancel={this.onCancelDelete} 
+                        okText="Yes" 
+                        cancelText="No"
+                      >
+                        <div className={`item-base delete round ${this.isMultiple(choosedList) ? 'active' : null}`}>
+                          <Icon type="delete" /> 批量删除
+                        </div>
+                      </Popconfirm> 
                     </Col>
                   </Row>
                 </Col>
@@ -217,10 +309,18 @@ export default class Gallery extends React.Component {
                       onClick={() => this.zoomImage(index)}
                       alt=""
                     />
+                    <div className="gallery-list__fileName">{item.fileName}</div>
                     <div className="gallery-list__tool-panel">
-                      <Icon type="file-text" onClick={(item) => this.copy(item)} />
-                      <Icon type="edit" onClick={(item) => this.openDialog(item)}/>
-                      <Icon type="delete" onClick={(item) => this.remove(item)} />
+                      <Icon type="file-text" onClick={() => this.copy(item)} />
+                      <Popconfirm 
+                        title="确定要删除这张图片吗" 
+                        onConfirm={() => this.onConfirm(item)} 
+                        onCancel={this.onCancel} 
+                        okText="Yes" 
+                        cancelText="No"
+                      >
+                        <Icon type="delete" />
+                      </Popconfirm>
                       <Checkbox class="pull-right" value={item.id} onChange={this.onCheckBoxChange} />
                     </div> 
                   </Col>
